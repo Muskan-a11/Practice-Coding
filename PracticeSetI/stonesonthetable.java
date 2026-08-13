9527d60350300e6dc67e5b3a226cde29906dbd11
MuskanXPlorer <th.utsavthakur@gmail.com>
2026-08-13 23:13:07 +0530

    Stones on the table

--- PracticeSetI/stonesonthetable.java
+++ PracticeSetI/stonesonthetable.java
@@ -1,7 +1,17 @@
 package PracticeSetI;
-
+import java.util.*;
 public class stonesonthetable {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.next();
+        int count = 0;
+        for(int i = 1;i < n;i++){
+            if(s.charAt(i) == s.charAt(i-1)){
+                count++;
+            }
+        }
+        System.out.println(count);
+        sc.close();
     }
 }
